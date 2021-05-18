9c16ceca356352b8447905a48d6620f5cf7c1f03
Thibaux Swildens <thibaux.swildens@gmail.com>
2021-05-18 23:35:05 +0200

    Fix

---
+++ src/components/Generator.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <section>
+      <header>
+
+      </header>
+      <main>
+<p>contenet</p>
+      </main>
+      <footer>
+
+      </footer>
+    </section>
+  </main>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Generator extends Vue {
+  
+}
+</script>
+
+<style scoped lang="scss">
+p {
+  background-color:#ff7b54;
+}
+
+</style>
